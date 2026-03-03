25e3c7177516d64bd4842d64211343dc31d0ef95
eadaradhiraj <eadaradhiraj@gmail.com>
2026-03-03 10:40:48 +0530

    Refactor dictionaries and improve kh2dev function

--- kh_to_dev.js
+++ kh_to_dev.js
@@ -1,138 +1,100 @@
+// --- DICTIONARIES ---
 const consonants_dict_dev = {
-    "k": "क",
-    "K": "ख",
-    "g": "ग",
-    "Q": "घ",
-    "G": "ङ",
-    "c": "च",
-    "C": "छ",
-    "j": "ज",
-    "Z": "झ",
-    "J": "ञ",
-    "T": "ट",
-    "V": "ठ",
-    "D": "ड",
-    "X": "ढ",
-    "N": "ण",
-    "t": "त",
-    "Y": "थ",
-    "d": "द",
-    "F": "ध",
-    "n": "न",
-    "p": "प",
-    "P": "फ",
-    "b": "ब",
-    "B": "भ",
-    "m": "म",
-    "y": "य",
-    "r": "र",
-    "l": "ल",
-    "v": "व",
-    "z": "श",
-    "S": "ष",
-    "s": "स",
-    "h": "ह"
-}
+    "k": "क", "K": "ख", "g": "ग", "Q": "घ", "G": "ङ",
+    "c": "च", "C": "छ", "j": "ज", "Z": "झ", "J": "ञ",
+    "T": "ट", "V": "ठ", "D": "ड", "X": "ढ", "N": "ण",
+    "t": "त", "Y": "थ", "d": "द", "F": "ध", "n": "न",
+    "p": "प", "P": "फ", "b": "ब", "B": "भ", "m": "म",
+    "y": "य", "r": "र", "l": "ल", "v": "व", "z": "श",
+    "S": "ष", "s": "स", "h": "ह"
+};
+
 const mahapranas_dict_dev = {
-    "ai": "E",
-    "au": "O",
-    "RR": "L",
-    "kh": "K",
-    "gh": "Q",
-    "ch": "C",
-    "jh": "Z",
-    "Th": "V",
-    "Dh": "X",
-    "th": "Y",
-    "dh": "F",
-    "ph": "P",
-    "bh": "B"
-}
+    "ai": "E", "au": "O", "RR": "L", "kh": "K", "gh": "Q", 
+    "ch": "C", "jh": "Z", "Th": "V", "Dh": "X", "th": "Y", 
+    "dh": "F", "ph": "P", "bh": "B"
+};
+
 const vowels_markers_dict_dev = {
-    'a': '',
-    'A': 'ा',
-    'i': 'ि',
-    'I': 'ी',
-    'u': 'ु',
-    'U': 'ू',
-    'R': 'ृ',
-    'L': 'ॄ',
-    'e': 'े',
-    'E': 'ै',
-    'o': 'ो',
-    'O': 'ौ',
-}
+    'a': '', 'A': 'ा', 'i': 'ि', 'I': 'ी', 'u': 'ु', 'U': 'ू',
+    'R': 'ृ', 'L': 'ॄ', 'e': 'े', 'E': 'ै', 'o': 'ो', 'O': 'ौ'
+};
 
 const actual_vowels_dict_dev = {
-    'a': 'अ',
-    'A': 'आ',
-    'i': 'इ',
-    'I': 'ई',
-    'u': 'उ',
-    'U': 'ऊ',
-    'R': 'ऋ',
-    'L': 'ॠ',
-    'e': 'ए',
-    'E': 'ऐ',
-    'o': 'ओ',
-    'O': 'औ',
-}
+    'a': 'अ', 'A': 'आ', 'i': 'इ', 'I': 'ई', 'u': 'उ', 'U': 'ऊ',
+    'R': 'ऋ', 'L': 'ॠ', 'e': 'ए', 'E': 'ऐ', 'o': 'ओ', 'O': 'औ'
+};
 
-const special_vowels_dict_dev = {
-    'M': 'ं',
-    'H': 'ः'
-}
-const space_period_dev = [' ', '.']
+const special_vowels_dict_dev = { 'M': 'ं', 'H': 'ः' };
+const space_period_dev = [' ', '.', '\n', '\t'];
 
-let consonant_dev_arr = Object.keys(consonants_dict_dev),
-    vowels_markers_dev_arr = Object.keys(vowels_markers_dict_dev),
-    special_vowels_dev_arr = Object.keys(special_vowels_dict_dev)
-    actual_vowels_dev_arr = Object.keys(actual_vowels_dict_dev)
-
-function kh2dev(orig) {
-    let transstr = "",
-        st = allreplace(orig, mahapranas_dict_dev),
-        fch = st.charAt('0'),
-        st_idx = 0
-    // if starting character is a vowel
-    if (actual_vowels_dev_arr.includes(fch)) {
-        transstr = actual_vowels_dict_dev[fch]
-        st_idx = 1
+// --- HELPERS ---
+function allreplace(retStr, obj) {
+    // Sort keys by length (longest first) to ensure "kh" is replaced before "k"
+    const keys = Object.keys(obj).sort((a, b) => b.length - a.length);
+    for (let x of keys) {
+        retStr = retStr.replace(new RegExp(x.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), 'g'), obj[x]);
     }
-    for (var i = st_idx; i < st.length; i++) {
-        let ch = st.charAt(i),
-            nch = st.charAt(i+1),
-            pch = st.charAt(i-1)
-        // if current letter is a vowel but previous is also a vowel or a psace
-        if (actual_vowels_dev_arr.includes(ch) && (
-            actual_vowels_dev_arr.includes(pch) || special_vowels_dev_arr.includes(pch) || space_period_dev.includes(pch)
-        )) {
-            transstr += actual_vowels_dict_dev[ch]
-        } 
-        // if current letter is a visarga or anunAsika
-        else if (special_vowels_dev_arr.includes(ch)) {
-            transstr += special_vowels_dict_dev[ch]
-            if (actual_vowels_dev_arr.includes(nch)) {
-                transstr += actual_vowels_dict_dev[nch]
-                i = i + 1
+    return retStr;
+}
+
+// Set up lookup sets for performance
+const consonant_keys = new Set(Object.keys(consonants_dict_dev));
+const vowel_marker_keys = new Set(Object.keys(vowels_markers_dict_dev));
+const actual_vowel_keys = new Set(Object.keys(actual_vowels_dict_dev));
+const special_vowel_keys = new Set(Object.keys(special_vowels_dict_dev));
+
+// --- MAIN FUNCTION ---
+function kh2dev(orig) {
+    // 1. Convert multi-letter sounds (kh -> K, ai -> E)
+    let st = allreplace(orig, mahapranas_dict_dev);
+    let transstr = "";
+
+    for (let i = 0; i < st.length; i++) {
+        let ch = st[i];
+        let nch = st[i + 1];
+        let pch = st[i - 1];
+
+        // A. Handle Consonants
+        if (consonant_keys.has(ch)) {
+            let baseConsonant = consonants_dict_dev[ch];
+
+            // If consonant is followed by a vowel marker (a, A, i, I, etc.)
+            if (nch && vowel_marker_keys.has(nch)) {
+                transstr += baseConsonant + vowels_markers_dict_dev[nch];
+                i++; // Consume the vowel marker
+            } 
+            // If consonant is followed by another consonant or end of word/string
+            else {
+                transstr += baseConsonant + '्';
             }
+        } 
+        
+        // B. Handle Vowels (Standalone: start of word or after space/other vowel)
+        else if (actual_vowel_keys.has(ch)) {
+            const isAfterConsonant = consonant_keys.has(pch);
+            
+            // If it's NOT after a consonant, it must be an actual standalone vowel
+            if (!isAfterConsonant) {
+                transstr += actual_vowels_dict_dev[ch];
+            }
+            // (If it WAS after a consonant, it was already handled by the logic in block A)
         }
-        // if consonant is followed by a consonant or end of string
-        else if
-            (consonant_dev_arr.includes(ch) && (consonant_dev_arr.includes(nch) || i == st.length - 1 || space_period_dev.includes(nch)) ) {
-            transstr += consonants_dict_dev[ch]+'्'
+
+        // C. Handle Special Vowels (Anusvara/Visarga)
+        else if (special_vowel_keys.has(ch)) {
+            transstr += special_vowels_dict_dev[ch];
         }
-        // if consonant followed by a vowel
-        else if (consonant_dev_arr.includes(ch) && vowels_markers_dev_arr.includes(nch)) {
-            transstr += consonants_dict_dev[ch] + vowels_markers_dict_dev[nch]
-            i = i + 1;
-        } else if (ch == "'") {
-            transstr += "ऽ"
-        } else if (ch == '.') {
-            transstr += "।"
+
+        // D. Handle Avagraha, Punctuation and Others
+        else if (ch === "'") {
+            transstr += "ऽ";
+        } else if (ch === ".") {
+            transstr += "।";
         } else {
-            transstr += ch
+            transstr += ch; // Keeps spaces, numbers, and newlines
         }
-      }
-    return transstr
+    }
+
+    return transstr;
 }
